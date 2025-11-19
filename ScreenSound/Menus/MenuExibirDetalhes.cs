14a1a0dd30a4eb99f70cd2a72046b09f1ffca2a1
DevSirq <joaosirqueirasantos@gmail.com>
2025-11-19 17:53:08 -0300

    Removendo o switch (deixando o código mais legível)

--- ScreenSound/Menus/MenuExibirDetalhes.cs
+++ ScreenSound/Menus/MenuExibirDetalhes.cs
@@ -4,10 +4,10 @@ namespace ScreenSound.Menus;
 
 internal class MenuExibirDetalhes : Menu
 {
-    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
-            Console.Clear();
-            ExibirTituloDaOpcao("Exibir detalhes da banda");
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Exibir detalhes da banda");
             Console.Write("Digite o nome da banda que deseja conhecer melhor: ");
             string nomeDaBanda = Console.ReadLine()!;
             if (bandasRegistradas.ContainsKey(nomeDaBanda))
